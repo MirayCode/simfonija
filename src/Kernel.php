88b314529c284e783fddbdbc64cac814bd934156
MirayCode <47726430+MirayCode@users.noreply.github.com>
2024-03-21 11:48:40 +0100

    Comments in the files about what is Kernel, personal notes

--- src/Kernel.php
+++ src/Kernel.php
@@ -9,3 +9,6 @@ class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 }
+
+//Kernel je neki dio programa koji se pokrece kada se pokrece aplikacija i sadrzi instrukcije za postavljanje
+// projekata
